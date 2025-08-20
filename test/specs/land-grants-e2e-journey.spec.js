3489c8ca24a167933bb6843bf2188f72e3f6d30f
Alistair Laing <aliuk2012@users.noreply.github.com>
2025-08-20 13:54:44 +0100

    Comment out some of the journey to test webdriver

--- test/specs/land-grants-e2e-journey.spec.js
+++ test/specs/land-grants-e2e-journey.spec.js
@@ -6,10 +6,10 @@ import {
 import {
   continueJourney,
   ensureUrl,
-  selectOption,
-  startJourney,
-  selectRequiredAction,
-  confirmAndSend
+  // selectOption,
+  startJourney
+  // selectRequiredAction,
+  // confirmAndSend
 } from '../utils/journey-actions.js'
 
 describe('Land grants end to end journey', () => {
@@ -36,39 +36,39 @@ describe('Land grants end to end journey', () => {
     // confirm your land details are up to date
     await ensureUrl('confirm-your-land-details-are-up-to-date')
     await analyseAccessibility()
-    await continueJourney()
+    // await continueJourney()
 
-    // select land parcel
-    await ensureUrl('select-land-parcel')
-    await analyseAccessibility()
-    await selectOption('SD6743 8083')
-    await continueJourney()
+    // // select land parcel
+    // await ensureUrl('select-land-parcel')
+    // await analyseAccessibility()
+    // await selectOption('SD6743 8083')
+    // await continueJourney()
 
-    // select action
-    await ensureUrl('select-actions-for-land-parcel')
-    await analyseAccessibility()
-    await selectRequiredAction('CMOR1')
-    await continueJourney()
+    // // select action
+    // await ensureUrl('select-actions-for-land-parcel')
+    // await analyseAccessibility()
+    // await selectRequiredAction('CMOR1')
+    // await continueJourney()
 
-    // // check selected land actions
-    await ensureUrl('check-selected-land-actions')
-    await analyseAccessibility()
-    await selectOption('No')
-    await continueJourney()
+    // // // check selected land actions
+    // await ensureUrl('check-selected-land-actions')
+    // await analyseAccessibility()
+    // await selectOption('No')
+    // await continueJourney()
 
-    // check summary
-    await ensureUrl('summary')
-    await analyseAccessibility()
-    await continueJourney()
+    // // check summary
+    // await ensureUrl('summary')
+    // await analyseAccessibility()
+    // await continueJourney()
 
-    // submit application
-    await ensureUrl('submit-your-application')
-    await analyseAccessibility()
-    await confirmAndSend()
+    // // submit application
+    // await ensureUrl('submit-your-application')
+    // await analyseAccessibility()
+    // await confirmAndSend()
 
-    // confirmation
-    await ensureUrl('confirmation')
-    await analyseAccessibility()
+    // // confirmation
+    // await ensureUrl('confirmation')
+    // await analyseAccessibility()
 
     generateAccessibilityReports('land-grants-e2e-journey')
   })
